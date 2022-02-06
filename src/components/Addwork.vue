2f646efd10bf3f40c8e528fa794e010c9cf96b35
Veerachai Seangkom <Devtee.pp@gmail.com>
2022-02-07 00:24:28 +0700

    first commit

---
+++ src/components/Addwork.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <h1>Homework / Assignment</h1>
+    <v-select :items="items" v-model="select"></v-select>
+    <v-btn @click="addTodo">click</v-btn>
+    <v-container>
+      <v-list v-for="(item, i) in additem" :key="i">
+     
+      </v-list>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Addwork.",
+  data() {
+    return {
+      select: "",
+      items: ["Web", "SA", "ML", "etc"],
+      additem: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.additem.push({ this.select });
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <Addwork />
+  </v-container>
+</template>
+<script>
+import Addwork from "../components/Addwork.vue";
+export default {
+  name: "Todo",
+  components: {
+    Addwork,
+  },
+};
+</script>
+
+<style></style>
